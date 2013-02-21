ff995fcb8cb5efbf9bbf063a0bc0be3a55330e52
mike brown <developer@entretec.com>
2013-02-21 20:52:10 +0800

    Initial Commit

---
+++ MatchismoCollection/collectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  collectionViewController.h
+//  MatchismoCollection
+//
+//  Created by mike on 21/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface collectionViewController : UIViewController
+
+@end
---
+++ MatchismoCollection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MatchismoCollection
+//
+//  Created by mike on 21/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "collectionAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([collectionAppDelegate class]));
+    }
+}
